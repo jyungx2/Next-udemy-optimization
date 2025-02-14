f4b9aa17c12ccee989f59b7ebb373bc8dd998c17
jiyoungnim <cloundyon31@gmail.com>
2025-02-14 20:40:08 +0900

    📝 199. Loading Unknown Images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 🚨 런타임에러 (hostname "res.cloudinary.com" is not configured under images in your `next.config.js`)에 대한 해결책
+  images: {
+    remotePatterns: [{ hostname: "res.cloudinary.com" }],
+  },
+};
 
 export default nextConfig;
