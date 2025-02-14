2b3100eedd85c689c7c6df0c577d82f2a30892c2
jiyoungnim <cloundyon31@gmail.com>
2025-02-14 21:24:02 +0900

    📝 201. Using An Image Loader & Cloudinary Resizing

--- components/posts.js
+++ components/posts.js
@@ -7,11 +7,34 @@ import LikeButton from "./like-icon";
 import { togglePostLikeStatus } from "@/actions/posts";
 import Image from "next/image";
 
+// Cloudinary 이미지 최적화를 위한 로더 함수
+// config 객체를 통해 src와 quality 값을 받아서 Cloudinary URL을 생성
+function imageLoader(config) {
+  const urlStart = config.src.split("upload/")[0];
+  const urlEnd = config.src.split("upload/")[1];
+
+  // Cloudinary 변환 파라미터 설정
+  // w_200: 너비 200px로 조정 & height 설정 시, 이미지 화질 깨지므로 자동설정되게끔 설정x
+  // q_${config.quality}: Image 컴포넌트의 quality prop 값으로 화질 조정
+  const transformations = `w_200,q_${config.quality}`;
+
+  // 최종 Cloudinary URL 생성 및 반환
+  return `${urlStart}upload/${transformations}/${urlEnd}`;
+}
+
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <Image src={post.image} fill alt={post.title} />
+        <Image
+          loader={imageLoader}
+          src={post.image}
+          // These width&height setting won't matter bc we were using Cloudinary for the image transformation and optimization anyways. (= width와 height는 Cloudinary에서 이미지 최적화를 하므로 실제로는 큰 의미가 없음)
+          width={200}
+          height={120}
+          alt={post.title}
+          quality={50} // 이미지 품질을 50%로 설정
+        />
       </div>
       <div className="post-content">
         <header>
